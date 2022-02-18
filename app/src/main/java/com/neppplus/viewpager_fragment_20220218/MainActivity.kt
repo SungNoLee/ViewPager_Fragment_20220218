a5152385efa596fbf88f24a0449c25ec0502158f
이 성 노 <kulbo0926@gmail.com>
2022-02-18 17:41:02 +0900

    ViewPage 일부완성

---
+++ app/src/main/java/com/neppplus/viewpager_fragment_20220218/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.neppplus.viewpager_fragment_20220218
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.neppplus.viewpager_fragment_20220218.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mAdapter: MainViewPagerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+        mAdapter = MainViewPagerAdapter( supportFragmentManager )
+        mainViewPager.adapter = mAdapter
+
+        mainViewPager.offscreenPageLimit = 3
+
+    }
+}
